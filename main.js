42285097ec3e7c39f1ce478f2b7bc10ca8cfaa0d
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-08-04 10:26:50 -0500

    refactored and edited

--- main.js
+++ main.js
@@ -1,41 +1,23 @@
-var button
-var onScreen
-var equals
-var parameter
-var memory
-var output
+var buttons = document.querySelectorAll(".button");
+var onScreen = document.querySelector(".screen");
+var clearButton = document.querySelector("#clear");
+var sqrtButton = document.querySelector("#sqrt");
+var equalsButton = document.querySelector(".equals");
 
-memory = []
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        if (!button.className.includes("noDisplay")) {
+            onScreen.innerHTML += button.innerHTML;
+        }
+    });
+});
 
-onScreen = document.querySelector(".screen")
-
-button = document.querySelectorAll(".button");
-var length = button.length;
-
-for (var i in button) {
-
-    if (button[i].innerHTML !== "C" && button[i].innerHTML !== "=" && button[i].innerHTML !== "√") {
-        button[i].addEventListener("click", function () {
-            num = this.innerHTML
-            output = onScreen.innerHTML += num;
-        });
-    } else if (button[i].innerHTML === "C") {
-        button[i].addEventListener("click", function () {
-            memory.push(onScreen.innerHTML);
-            onScreen.innerHTML = ""
-        });
-
-    } else if (button[i].innerHTML === "=") {
-        button[i].addEventListener("click", function () {
-            onScreen.innerHTML = eval(onScreen.innerHTML)
-
-        });
-    }
-    else if (button[i].innerHTML === "√") {
-        button[i].addEventListener("click", function () {
-            onScreen.innerHTML = eval(Math.sqrt(onScreen.innerHTML))
-        });
-    }
-}
-
-
+clearButton.addEventListener("click", function (e) {
+    onScreen.innerHTML = "";
+});
+sqrtButton.addEventListener("click", function (e) {
+    onScreen.innerHTML = eval(Math.sqrt(onScreen.innerHTML));
+});
+equalsButton.addEventListener("click", function (e) {
+    onScreen.innerHTML = eval(onScreen.innerHTML);
+});
